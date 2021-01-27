0506fad44a5819acc2c1c0bf077c0c2620d50989
Тимофей <tymcrit@gmail.com>
2021-01-27 17:31:13 +0300

    README code test is added

--- src/lib.rs
+++ src/lib.rs
@@ -85,7 +85,7 @@ impl<T> PersistentStack<T> {
     }
 
     /// Pops value from stack and tries to return it
-    /// 
+    ///
     /// Returns `Ok(data)` if only this copy of stack owned `data`
     ///
     /// Returns `Err(CantUnwrap)` if there are other copies, which own `data`
@@ -110,14 +110,13 @@ impl<T> PersistentStack<T> {
     /// assert_eq!(s3.iter().copied().collect::<Vec<_>>(), vec![5, 1]); // s3 is cloned, when s2 was [1]
     pub fn pop(&mut self) -> Result<T, PersistentStackPopError> {
         let s = self.0.take();
-        s
-            .map(|node| {
-                self.0 = node.parent.as_ref().cloned();
-                Arc::try_unwrap(node)
-                    .map(|node| node.data)
-                    .map_err(|_| PersistentStackPopError::CantUnwrap)
-            })
-            .unwrap_or(Err(PersistentStackPopError::RootIsReached))
+        s.map(|node| {
+            self.0 = node.parent.as_ref().cloned();
+            Arc::try_unwrap(node)
+                .map(|node| node.data)
+                .map_err(|_| PersistentStackPopError::CantUnwrap)
+        })
+        .unwrap_or(Err(PersistentStackPopError::RootIsReached))
     }
 
     /// Creates iterator over `self` by reference
@@ -170,5 +169,35 @@ mod tests {
         s2.push(4);
         assert_eq!(s1.into_iter().map(|x| *x).collect::<Vec<_>>(), [3, 2, 1]);
         assert_eq!(s2.into_iter().map(|x| *x).collect::<Vec<_>>(), [4, 2, 1]);
+    }
+
+    #[test]
+    fn readme_example() {
+        use crate::PersistentStack;
+
+        let mut s1 = PersistentStack::new();
+        s1.push(1);
+
+        let mut s2 = s1.clone(); // O(1) operation
+
+        std::thread::spawn(move || {
+            s2.push(2); // PersistentStack values can be shared safely
+            assert_eq!(s2.iter().copied().collect::<Vec<_>>(), vec![2, 1]);
+
+            std::thread::sleep(std::time::Duration::from_millis(20));
+
+            s2.push(4);
+            assert_eq!(s2.iter().copied().collect::<Vec<_>>(), vec![4, 2, 1]);
+
+            assert_eq!(s2.pop(), Ok(4)); // We can also pop values from stack
+        });
+
+        s1.push(3);
+        assert_eq!(s1.iter().copied().collect::<Vec<_>>(), vec![3, 1]);
+
+        std::thread::sleep(std::time::Duration::from_millis(20));
+
+        s1.push(5);
+        assert_eq!(s1.iter().copied().collect::<Vec<_>>(), vec![5, 3, 1]);
     }
 }
